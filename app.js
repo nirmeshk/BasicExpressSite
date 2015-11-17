1e1a84c6cdb59d65f014c7286407102ef604c2a2
Trey Moore <dana.moore@bronto.com>
2015-11-16 21:01:38 -0500

    Redis Feature Flags

--- app.js
+++ app.js
@@ -4,9 +4,14 @@
 var express = require('express')
   , stylus = require('stylus')
   , nib = require('nib')
+  , redis = require('redis')
 
 
 var app = express()
+var redisConf = require('./redisConf.js').redisConf;
+var redisClient = redis.createClient(6379, '107.170.29.129', redisConf);
+var featureFlagsKey = 'featureFlags';
+var newTitleFeatureFlag = 'newTitle';
 
 function compile(str, path) {
   return stylus(str)
@@ -25,9 +30,17 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
+  redisClient.sismember(featureFlagsKey, newTitleFeatureFlag, function(err, result) {
+      if(err) {
+        console.log("Error in redis connection: " + err);
+        res.render('index', { title : 'Home'});
+      }
+      if(result) {
+        res.render('index', { title : 'New Title'});
+      } else {
+        res.render('index', { title : 'Home'});
+      }
+  });
 })
 
 app.listen(3000)
