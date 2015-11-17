700f890549841977313a8971f649cfa409f07fd9
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-11-17 12:07:05 -0500

    updating buildServer.js

--- buildServer.js
+++ buildServer.js
@@ -1,22 +1,29 @@
 var needle = require('needle');
 var fs = require('fs')
 var ini = require('ini')
+var express = require('express');
+var redis = require('redis');
+var app = express();
+
+
 
 config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
 
+var redisClient = redis.createClient(6379, '107.170.29.129', config.auth.redis_pass);
+
 var authToken = config.auth.travis_auth_token
-console.log(authToken)
 
-var options = {
-  headers: { 'Content-Type':'application/json' }
-}
+app.get('/build', function (req, res) {
+  // verify the auth token
+  res.send('Hello World!');
+  console.log(req.params.authToken);
+  console.log(req.params.branch);
+  console.log(req.params.commit);
+  // Verify the branch to be deployed. We are assuming that only `master` branch is deployed
+});
 
-var host = 'https://api.travis-ci.org/repos/nirmeshk/BasicExpressSite/builds/91501273'
-
-needle.get(host, options, function(error, response) {
-  if (!error && response.statusCode == 200){
-    //console.log(response.body); // JSON decoding magic. :)
-  } else {
-    //console.log(error)
-  }
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Deployment server listening at http://%s:%s', host, port);
 });
